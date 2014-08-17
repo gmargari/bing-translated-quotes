bad8961c240623c0850aa2f073518257d947bbbd
Giorgos Margaritis <gmargari@cs.uoi.gr>
2014-08-17 23:19:44 +0300

    jquery is loaded from local file.

--- create-html.py
+++ create-html.py
@@ -30,7 +30,7 @@ footer = """
         </div>
       </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="js/jquery-1.7.1.min.js"></script>
     <script src="js/jquery.rs.slideshow.min.js"></script>
     <script src="js/dynamic-controls-bootstrap.js"></script>
   </body>
