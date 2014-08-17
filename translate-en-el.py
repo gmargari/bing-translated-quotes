23557aadb6aea2ab10c50ac72b2d80bf4f1a6b56
Giorgos Margaritis <gmargari@cs.uoi.gr>
2014-08-17 17:00:50 +0300

    Quotes are translated in batches.

--- translate-en-el.py
+++ translate-en-el.py
@@ -15,6 +15,8 @@ outFilename = "quotes.el.txt"
 # translateText()
 #==============================================================================
 def translateText(text):
+    print "Translating buffer..."
+
     # Source: http://stackoverflow.com/a/12149985
     args = {
         'client_id': clientID,
@@ -33,31 +35,68 @@ def translateText(text):
     translation_args['text'] = text
     result = requests.get(translation_url + urllib.urlencode(translation_args),
                           headers = headers)
-    return result.content
+    text = result.content
+
+    # Remove the first 4 bytes, <feff>, defining the Byte Order Marker
+    text = text[3:]
+
+    # Remove the trailing and leading quotes added by Bing
+    text = text[1:-1]
+
+    return text
 
 #==============================================================================
 # main()
 #==============================================================================
 def main():
-    outFile = open(outFilename, "w")
-    with open (inFilename, "r") as myfile:
-        quotes = 1
-        for line in myfile:
-            # Each line is a pair of: <quote text> # <author name>
+    with open (inFilename, "r") as inFile:
+
+        # To avoid translating the file line-by-line (i.e. quote-by-quote),
+        # create a buffer that contains the text of multiple quotes and send
+        # this buffer for translation.
+        text_buffer = ""
+        whole_translated_text = ""
+        for line in inFile:
+
+            # Each line of input file is a pair of: <quote text> # <author name>
             quote = line.split("#")
             if (len(quote) > 1):
                 text = quote[0].strip()
                 author = quote[1].strip()
-                print "Translating quote " + str(quotes)
-                translated_text = translateText(text)
-                # [4:]: remove the first 4 bytes, <feff>, defining the Byte Order Marker
-                outFile.write("{:s} # {:s}\n".format(translated_text[3:], author))
-                quotes = quotes + 1
-    outFile.close()
+                text_buffer = text_buffer + text + "\n"
+                
+                # If buffer is long enough, translate it and add it to the
+                # buffer containg the translated text
+                if (len(text_buffer) > 8192):
+                    translated_text = translateText(text_buffer)
+                    whole_translated_text = whole_translated_text + translated_text
+                    text_buffer = ""
+
+        # Translate any remaining text in buffer
+        if (len(text_buffer) > 0):
+            translated_text = translateText(text_buffer)
+            whole_translated_text = whole_translated_text + translated_text
+
+        # Combine the translated quotes with their respective authors and
+        # write them in output file
+        with open (outFilename, "w") as outFile:
+
+            # Split lines by the unicode character \u000a (Line feed)
+            translated_text_lines = whole_translated_text.split('\u000a')
+            print "lines = ", len(translated_text_lines)
+
+            inFile.seek(0)
+            i = 0
+            for line in inFile:
+                print i,
+                quote = line.split("#")
+                if (len(quote) > 1):
+                    text = quote[0].strip()
+                    author = quote[1].strip()
+                    translated_text = translated_text_lines[i]
+                    outFile.write("{:s}#{:s}\n".format(translated_text, author))
+                    i = i + 1
 
 if __name__ == "__main__":
     main()
-
-# Note: quotes.en.txt is the result of cleaning and processing:
-# https://github.com/mjseaman/restful_crud/blob/master/db/litemind-quotes.csv
 
