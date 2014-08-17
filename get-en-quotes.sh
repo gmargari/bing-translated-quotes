164c838c4d2b84d303c6107f79223c40e33b07fe
Giorgos Margaritis <gmargari@cs.uoi.gr>
2014-08-17 16:52:59 +0300

    Added script to get english quotes from various places.

---
+++ get-en-quotes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function get_quotes_1() {
+    wget -qO- https://raw.githubusercontent.com/wp-plugins/famous-quotes/master/quotes.xml | \
+        awk '
+        /<author>/ {
+            gsub("<[^>]*>", "", $0)     # remove html tags
+            gsub("^[ \t]*", "", $0)     # remove trailing white space
+            author = $0
+        }
+        /<text>/ {
+            gsub("<[^>]*>", "", $0)     # remove html tags
+            gsub("^[ \t]*", "", $0)     # remove trailing white space
+            gsub("\"", "", $0)          # remove quotes
+            text = $0
+            printf "%s#%s\n", text, author 
+        }
+        '
+}
+
+function get_quotes_2() {
+    wget -qO- http://www-public.rz.uni-duesseldorf.de/~puschman/MySig/data/famousquotes.txt |
+        awk -F "<br>- " ' NR > 2 {
+            text = $1
+            author = $2
+            gsub("<br>", " ", text)     # replace <br>s inside text with spaces    
+            gsub("#", " ", text)        # remove #s from text    
+            gsub("\"", "", text)        # remove quotes
+            gsub("<br>", " ", author)   # replace <br>s inside author with spaces    
+            printf "%s#%s\n", text, author
+        }'
+}
+
+get_quotes_1 >  quotes.en.txt
+get_quotes_2 >> quotes.en.txt
+
