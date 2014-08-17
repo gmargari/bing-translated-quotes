0e2e99ff9403675887fe2cf7606ef3df421fca05
Giorgos Margaritis <gmargari@cs.uoi.gr>
2014-08-17 17:32:17 +0300

    Updated script that creates quote images.

--- create-quote-images.py
+++ create-quote-images.py
@@ -6,17 +6,39 @@ import ImageDraw
 import textwrap
 import os
 
-quoteTextFont = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Oblique.ttf", 26)
-quoteAuthorFont = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf", 26)
-quoteLeftMargin = 50
-textTopMargin = 120
-authorTopSpace = 40
+quoteTextFont = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Oblique.ttf", 24)
+quoteAuthorFont = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf", 24)
+quoteLeftMargin = 40       # space from left side, in pixels
+textWrapWidth = 42         # Max width of wrapped lines, in characters
 quoteTextColor = "#eeeeee"
 quoteAuthorColor = "#eeeeee"
 bgImage = './bg.jpg'
-outFolder = "./quotes"     # Will be created if it does not exis
+outFolder = "./images/"     # Will be created if it does not exis
 outImagePrefix = "quote-"
 outImageExtension = ".jpg"
+
+#==============================================================================
+# lineHeight()
+#==============================================================================
+def lineHeight(textFont, text):
+    (width, height) = textFont.getsize(text)
+    return height * 1.1
+
+#==============================================================================
+# topMargin()
+#==============================================================================
+def topMargin(text, font, draw, img):
+    (imgW, imgH) = img.size
+    max_lines = imgH / lineHeight(font, text)
+    # +2: one line for author and one empty line between text and author
+    text_lines = len(textwrap.wrap(text, width = textWrapWidth)) + 2
+    return ((max_lines - text_lines) / 2) * lineHeight(font, text)
+
+#==============================================================================
+# addText()
+#==============================================================================
+def addText(draw, x, y, text, textFont, textColor):
+    draw.text((x, y), text, font = textFont, fill = textColor)
 
 #==============================================================================
 # createQuoteImage()
@@ -24,18 +46,19 @@ outImageExtension = ".jpg"
 def createQuoteImage(quoteText, quoteAuthor, bgImgFilename, outFilename):
 
     # Open background image
-    img = Image.open(bgImgFilename)
+    img = Image.open(bgImgFilename)            
     draw = ImageDraw.Draw(img)
 
     # Add text to image
     quoteText = unicode(quoteText, 'utf-8')
     quoteAuthor = unicode(quoteAuthor, 'utf-8')
     margin = quoteLeftMargin
-    offset = textTopMargin
-    for line in textwrap.wrap(quoteText, width=45):
-        draw.text((margin, offset), line, font = quoteTextFont, fill = quoteTextColor)
-        offset += quoteTextFont.getsize(line)[1]
-    draw.text((margin, offset + authorTopSpace), quoteAuthor, font = quoteAuthorFont, fill = quoteAuthorColor)
+    offset = topMargin(quoteText, quoteTextFont, draw, img)
+    for line in textwrap.wrap(quoteText, width = textWrapWidth):
+        addText(draw, margin, offset, line, quoteTextFont, quoteTextColor)
+        offset += lineHeight(quoteTextFont, line)
+    offset += lineHeight(quoteTextFont, quoteAuthor) # one empty line before author
+    addText(draw, margin, offset, quoteAuthor, quoteAuthorFont, quoteAuthorColor)
     del draw
 
     # Save result image
@@ -49,17 +72,17 @@ def main():
         # Create output folder, if it does not exist
         if not os.path.exists(outFolder):
             os.makedirs(outFolder)
-        quotes = 1
+        num = 1
         for line in myfile:
             # Each line is a pair of: <quote text> # <author name>
             quote = line.split("#")
             if (len(quote) > 1):
                 text = quote[0].strip()
                 author = quote[1].strip()
-                outputFile = outFolder + "/" + outImagePrefix + str(quotes) + outImageExtension
+                outputFile = outFolder + "/" + outImagePrefix + str(num) + outImageExtension
                 print "Creating image " + outputFile + " ..."
                 createQuoteImage(text, author, bgImage, outputFile)
-                quotes = quotes + 1
+                num = num + 1
 
 if __name__ == "__main__":
     main()
